050d9a73e3e6ff604e302744a9c53487e812b263
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-03 21:15:30 +0300

    WIP: add model Note

--- altikaApp/backend/models.py
+++ altikaApp/backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
