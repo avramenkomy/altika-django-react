4d87feabd88d75e657350e7e90a8d672e92baee9
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-03 21:29:14 +0300

    WIP: connect rest_framework to project

--- altikaApp/altikaApp/settings.py
+++ altikaApp/altikaApp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #myApps
+    # library
+    'rest_framework',
+    # myApps
     'backend'
 ]
 
