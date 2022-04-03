865366d8a694d43068d78cca9d42647a89a49c30
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-03 21:37:50 +0300

    WIP: add view

--- altikaApp/backend/views.py
+++ altikaApp/backend/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
