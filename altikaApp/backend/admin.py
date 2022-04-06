260da537170ba11cd4aaf685d14762d924e83677
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-06 21:39:43 +0300

    WIP: register email model in admin panel

--- altikaApp/backend/admin.py
+++ altikaApp/backend/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from backend.models import Note
+from backend.models import Note, EmailMessage
 
 
 # Register your models here.
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EmailMessage)
+class EmailMessageAdmin(admin.ModelAdmin):
     pass
