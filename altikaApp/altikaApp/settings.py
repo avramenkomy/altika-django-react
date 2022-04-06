76a2aaf184d590eab165fe9e1c820f9fc9fb0ed3
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-06 21:42:34 +0300

    WIP: add settings for smtp email server

--- altikaApp/altikaApp/settings.py
+++ altikaApp/altikaApp/settings.py
@@ -130,3 +130,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
