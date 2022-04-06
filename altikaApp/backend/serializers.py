993d7056ad8032a74b575c53bbfc20b0e6c67dc6
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-06 21:32:12 +0300

    WIP: add email serializer

--- altikaApp/backend/serializers.py
+++ altikaApp/backend/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Note
+from .models import Note, EmailMessage
 
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ('id', 'title', 'content', 'author', 'created_at')
+
+
+class EmailMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailMessage
+        fields = ('id', 'subject', 'message', 'to_send', 'create_date', 'send_date', 'status')
