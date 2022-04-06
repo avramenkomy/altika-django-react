6def6927e10280f3f8b9e0bfc1f4c1e46cd1d65d
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-06 21:35:16 +0300

    WIP: add url for send email

--- altikaApp/backend/urls.py
+++ altikaApp/backend/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.front, name='front'),
     path('api/note/', views.NoteListCreate.as_view()),
+    path('api/email_call_us/', views.EmailMessageListCreate.as_view()),
     path('notes/', views.note, name='note'),
+    path('send/', views.email_call_us, name='email_call_us')
 ]
