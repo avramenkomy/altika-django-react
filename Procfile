863ddf3cc2096de1a0d83d86c3ba8c65b05d4669
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-07 19:25:19 +0300

    WIP: new settings for deploy to heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python altikaApp/manage.py migrate && cd frontend && npm run build
-web: python altikaApp/manage.py runserver 0.0.0.0:$PORT
+release: python altikaApp/manage.py migrate
+web: gunicorn altikaApp.wsgi --log-file -
--- altikaApp/altikaApp/settings.py
+++ altikaApp/altikaApp/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
